208f05d5c9b28d6d969380c7a38b5dee0e3aac55
Zach Daniel <zach@zachdaniel.dev>
2026-04-26 14:23:07 -0400

    chore: refactor notifier to avoid compile warning

--- dev/notifier.ex
+++ dev/notifier.ex
@@ -2,18 +2,16 @@ defmodule Demo.BlobNotifier do
   @moduledoc false
   use Ash.Notifier
 
-  @impl true
-  def notify(%Ash.Notifier.Notification{action: %{name: :run_pending_analyzers}} = notification) do
     if Code.ensure_loaded?(Phoenix.PubSub) do
-      Phoenix.PubSub.broadcast(
-        Demo.PubSub,
-        "blob_analysis",
-        {:analysis_complete, notification.data}
-      )
-    else
-      :ok
+      @impl true
+      def notify(%Ash.Notifier.Notification{action: %{name: :run_pending_analyzers}} = notification) do
+          Phoenix.PubSub.broadcast(
+            Demo.PubSub,
+            "blob_analysis",
+            {:analysis_complete, notification.data}
+          )
+      end
     end
-  end
 
   def notify(_), do: :ok
 end
