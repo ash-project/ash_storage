964d6b90f0d89167f0b682750837b3ce08f7a18e
ken-kost <kenneth.kostresevic@gmail.com>
2026-05-10 11:14:14 +0200

    Expand test support

--- test/support/oban/pg_post.ex
+++ test/support/oban/pg_post.ex
@@ -19,6 +19,32 @@ defmodule AshStorage.Test.PgPost do
       variant(:eager_upper, AshStorage.Test.UppercaseVariant, generate: :eager)
       variant(:oban_upper, AshStorage.Test.UppercaseVariant, generate: :oban)
       variant(:oban_reverse, AshStorage.Test.ReverseVariant, generate: :oban)
+
+      variant(:oban_grouped_a, {AshStorage.Test.UppercaseVariant, suffix: ":a"},
+        generate: :oban,
+        group: :fast
+      )
+
+      variant(:oban_grouped_b, {AshStorage.Test.UppercaseVariant, suffix: ":b"},
+        generate: :oban,
+        group: :fast
+      )
+
+      variant(:oban_later, AshStorage.Test.ReverseVariant, generate: :oban, order: 1)
+
+      variant(:oban_extra_late, AshStorage.Test.UppercaseVariant, generate: :oban, order: 1)
+
+      variant(:oban_slow_x, {AshStorage.Test.UppercaseVariant, suffix: ":x"},
+        generate: :oban,
+        group: :slow,
+        order: 1
+      )
+
+      variant(:oban_slow_y, {AshStorage.Test.UppercaseVariant, suffix: ":y"},
+        generate: :oban,
+        group: :slow,
+        order: 1
+      )
     end
 
     has_many_attached(:documents, dependent: :detach)
